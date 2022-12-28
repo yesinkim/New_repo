b8bc0b2c50239d464eacbad35bf0fc3237ae6cb4
yesinkim <bailando.ys@gmail.com>
2022-12-28 18:55:11 +0900

    [Style] Apply black, isort

--- rnn.py
+++ rnn.py
@@ -1,5 +1,6 @@
 import math
 from typing import Optional, Tuple
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -7,17 +8,19 @@ from torch import Tensor
 
 class RNNBase(nn.Module):
     """Base class for advanced RNN Module"""
-    __constants__ = ['input_size', 'output_size', 'bias']
+
+    __constants__ = ["input_size", "output_size", "bias"]
 
     def __init__(
-        self, 
-        input_size: int, 
-        hidden_size: int, 
+        self,
+        input_size: int,
+        hidden_size: int,
         bias: bool,
         num_chunks: int,
-        nonlinearity: str ='tanh',
-        device = None,
-        dtype = None) -> None:
+        nonlinearity: str = "tanh",
+        device=None,
+        dtype=None,
+    ) -> None:
         super().__init__()
         factory_kwargs = {"device": device, "dtype": dtype}
         self.input_size = input_size
@@ -25,20 +28,24 @@ class RNNBase(nn.Module):
         self.bias = bias
         self.num_chunks = num_chunks
         self.nonlinearity = nonlinearity
-        if self.nonlinearity not in ['tanh', 'relu']:
-            raise ValueError("Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``")
+        if self.nonlinearity not in ["tanh", "relu"]:
+            raise ValueError(
+                "Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``"
+            )
 
         self.ih = nn.Linear(
-            in_features=input_size, 
-            out_features=num_chunks * hidden_size, 
-            bias=bias, 
-            **factory_kwargs)
+            in_features=input_size,
+            out_features=num_chunks * hidden_size,
+            bias=bias,
+            **factory_kwargs
+        )
 
         self.hh = nn.Linear(
-            in_features=hidden_size, 
-            out_features=num_chunks * hidden_size, 
-            bias=bias, 
-            **factory_kwargs)
+            in_features=hidden_size,
+            out_features=num_chunks * hidden_size,
+            bias=bias,
+            **factory_kwargs
+        )
 
         self.reset_parameters()
 
@@ -49,6 +56,8 @@ class RNNBase(nn.Module):
 
 
 nn.RNN
+
+
 class RNNCell(RNNBase):
     """An Elman RNN cell with tanh or ReLU non-linearity.
 
@@ -61,55 +70,61 @@ class RNNCell(RNNBase):
     Inputs: input, hidden
         - input: tensor containing the input features
         - hidden: tensor containing the initial hidden state
-        """
+    """
 
     def __init__(
-        self, 
-        input_size: int, 
-        hidden_size: int, 
-        bias: bool,
-        device = None,
-        dtype = None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super().__init__(input_size=input_size, hidden_size=hidden_size, bias=bias, num_chunks=1,  **factory_kwargs)
+        super().__init__(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            bias=bias,
+            num_chunks=1,
+            **factory_kwargs
+        )
 
     def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         # Make zero tensor if tensor is not initialized
         if hx is None:
-            hx = torch.zeros(input.size(0), self.hidden_size, dtype=input.dtype, device=input.device)
-        
+            hx = torch.zeros(
+                input.size(0), self.hidden_size, dtype=input.dtype, device=input.device
+            )
+
         # forward
         hy = self.ih(input) * self.hh(hx)
 
         # function
-        if self.nonlinearity == 'tanh':
+        if self.nonlinearity == "tanh":
             ret = torch.tanh(hy)
-        
+
         else:
             ret = torch.relu(hy)
 
         return ret
 
 
-# nn.LSTMCell 
+# nn.LSTMCell
 class LSTMCell(RNNBase):
     def __init__(
-        self, 
-        input_size: int, 
-        hidden_size: int, 
-        bias: bool,
-        device = None,
-        dtype = None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super(LSTMCell, self).__init__(input_size, hidden_size, bias=bias, num_chunks=4, **factory_kwargs)
+        super(LSTMCell, self).__init__(
+            input_size, hidden_size, bias=bias, num_chunks=4, **factory_kwargs
+        )
 
-    def forward(self, input: Tensor, hx: Optional[Tuple[Tensor, Tensor]] = None) -> Tuple[Tensor, Tensor]:
+    def forward(
+        self, input: Tensor, hx: Optional[Tuple[Tensor, Tensor]] = None
+    ) -> Tuple[Tensor, Tensor]:
         if hx is None:
-            hx = torch.zeros(input.size(0), self.hidden_size, dtype=input.dtype, device=input.device)
+            hx = torch.zeros(
+                input.size(0), self.hidden_size, dtype=input.dtype, device=input.device
+            )
             hx = (hx, hx)
-        
+
         hx, cx = hx
-        gates = self.ih(input) * self.hh(hx)    # first layer
+        gates = self.ih(input) * self.hh(hx)  # first layer
         input_gate, forget_gate, cell_gate, output_gate = gates.chunk(4, 1)
 
         forget_layer = torch.sigmoid(forget_gate)
@@ -122,22 +137,28 @@ class LSTMCell(RNNBase):
 
         return (h_t, c_t)
 
+
 # nn.GRU
 class GRUCell(RNNBase):
     def __init__(
-        self, 
-        input_size: int, 
-        hidden_size: int, 
-        bias: bool,
-        device = None,
-        dtype = None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super(GRUCell, self).__init__(input_size, hidden_size, bias=bias, num_chunks=3, **factory_kwargs)
+        super(GRUCell, self).__init__(
+            input_size, hidden_size, bias=bias, num_chunks=3, **factory_kwargs
+        )
 
-    def forward(self, input: Tensor, hx: Optional[Tuple[Tensor, Tensor]] = None) -> Tuple[Tensor, Tensor]:
+    def forward(
+        self, input: Tensor, hx: Optional[Tuple[Tensor, Tensor]] = None
+    ) -> Tuple[Tensor, Tensor]:
         if hx is None:
-            hx = torch.zeros(input.size(0), self.num_chunks * self.hidden_size, dtype=input.dtype, device=input.device)
-        
+            hx = torch.zeros(
+                input.size(0),
+                self.num_chunks * self.hidden_size,
+                dtype=input.dtype,
+                device=input.device,
+            )
+
         x_t = self.ih(input)
         h_t = self.hh(hx)
 
@@ -148,12 +169,12 @@ class GRUCell(RNNBase):
         update_gate = torch.sigmoid(x_update, h_update)
         new_gate = torch.tanh(reset_gate * h_new + x_new)
 
-        h_t = (1-update_gate) * new_gate + update_gate * hx
+        h_t = (1 - update_gate) * new_gate + update_gate * hx
 
         return h_t
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # RNN cell
     rnn = GRUCell(10, 20, True)
     print(rnn.parameters)
